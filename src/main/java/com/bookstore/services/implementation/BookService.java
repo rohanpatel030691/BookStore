8b6e50f1bea35447d911300d5351e3da30ad6ee8
rohanpatel030691 <138220846+rohanpatel030691@users.noreply.github.com>
2023-07-20 11:46:02 +0530

    Added logs

--- src/main/java/com/bookstore/services/implementation/BookService.java
+++ src/main/java/com/bookstore/services/implementation/BookService.java
@@ -13,6 +13,8 @@ import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,6 +24,8 @@ import java.util.Optional;
 
 @Service
 public class BookService implements IBookService {
+
+    private static final Logger logger = LoggerFactory.getLogger(BookService.class);
 
     @PersistenceContext
     public EntityManager entityManager;
@@ -35,39 +39,48 @@ public class BookService implements IBookService {
 
     @Override
     public BookDTO addBook(BookDTO bookDTO) {
+        logger.info("Adding a new book: {}", bookDTO);
         Book book = new Book(bookDTO);
         return BookMapper.toDTO(bookRepository.save(book));
     }
 
     @Override
     public BookDTO updateBook(Long bookId, BookDTO bookDTO) {
+        logger.info("Updating book with ID {}: {}", bookId, bookDTO);
         Optional<Book> optionalBook = bookRepository.findById(bookId);
         if (optionalBook.isPresent()) {
             Book existingBook = optionalBook.get();
             BookMapper.updateBookFromDTO(existingBook, bookDTO);
             existingBook = bookRepository.save(existingBook);
+            logger.info("Updated book: {}", existingBook);
             return BookMapper.toDTO(existingBook);
         } else {
             // Book with the given ID does not exist
+            logger.warn("Book with ID {} not found", bookId);
             throw new BookNotFoundException("Book with given ID does not exist in the database.");
         }
     }
 
     @Override
     public Boolean removeBook(Long bookId) {
+        logger.info("Removing book with ID: {}", bookId);
         Optional<Book> optionalBook = bookRepository.findById(bookId);
         if (optionalBook.isPresent()) {
             Book existingBook = optionalBook.get();
             bookRepository.delete(existingBook);
+            logger.info("Book with ID {} removed successfully", bookId);
             return true;
         } else {
             // Book with the given ID does not exist
+            logger.warn("Book with ID {} not found", bookId);
             throw new BookNotFoundException("Book with given ID does not exist in the database.");
         }
     }
 
     @Override
     public List<BookDTO> getBooks(String type,int volume,String author,String genre,String excludeGenre) {
+        logger.info("Fetching books with parameters: type={}, volume={}, author={}, genre={}, excludeGenre={}",
+                type, volume, author, genre, excludeGenre);
         List<Book> books = filterBooks(type,volume,genre,author,excludeGenre);
 
         List<BookDTO> bookDTOs = new ArrayList<>();
@@ -76,6 +89,7 @@ public class BookService implements IBookService {
             BookDTO bookDTO = BookMapper.toDTO(book);
             bookDTOs.add(bookDTO);
         }
+        logger.info("Found {} books matching the criteria", bookDTOs.size());
         return bookDTOs;
     }
 
